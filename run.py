fb40ba0ea208dd63f3c974b557138038f7c52f9b
Platon Anufriev <49365342+Haskeri@users.noreply.github.com>
2026-06-09 11:30:00 +0300

    Фикс запуска: убрал if __name__, добавил host 0.0.0.0

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=5000)
+with app.app_context():
+    db.create_all()
+
+app.run(debug=True, port=5000, host='0.0.0.0')
